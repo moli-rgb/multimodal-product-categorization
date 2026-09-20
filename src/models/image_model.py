fix: enable gradient updates for layer4 in ImageClassifier and adjust optimizer configuration

src/models/image_model.py
@@ -11,8 +11,8 @@ class ImageClassifier(nn.Module):
     def __init__(self,num_classes):
         super().__init__()
         self.backbone = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-        for param in self.backbone.parameters():
-            param.requires_grad = False
+        for param in self.backbone.layer4.parameters():
+            param.requires_grad = True
         self.backbone.fc = nn.Linear(self.backbone.fc.in_features, num_classes)
     def forward(self, x):
         return self.backbone(x)
@@ -40,7 +40,10 @@ if __name__ == "__main__":
 
     model = ImageClassifier(num_classes=len(category2idx)).to(device)
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.backbone.fc.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam([
+    {'params': model.backbone.layer4.parameters(), 'lr': 0.0001},
+    {'params': model.backbone.fc.parameters(), 'lr': 0.001}
+])
 
     num_epochs = 10
     best_f1 = 0
@@ -67,7 +70,7 @@ if __name__ == "__main__":
         if val_f1 > best_f1:
             best_f1 = val_f1
             epochs_without_improvement = 0
-            torch.save(model.state_dict(), '/content/drive/MyDrive/raw_data/best_image_model.pth')
+
             print(f"  → New best model saved! F1: {best_f1:.4f}")
         else:
             epochs_without_improvement += 1